1459245f67626c2c60df3825af856f86171e7e9c
Mahmud-007 <mahmud6799@gmail.com>
2025-12-13 22:54:12 +0600

    Updates

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("E:\\Projects\\Mahmud-portfolio\\.cache\\dev-404-page.js")),
+  "component---src-pages-index-tsx": preferDefault(require("E:\\Projects\\Mahmud-portfolio\\src\\pages\\index.tsx"))
+}
+
